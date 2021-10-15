ed8f807a357c959e879fc1def832dca471b44cea
jdlovin <josh.lovin@revature.net>
2021-10-14 20:26:42 -0400

    getting customer configured to the gateway but it keeps shutting down

--- Customer/src/test/java/com/nicky/customer/CustomerApplicationTests.java
+++ Customer/src/test/java/com/nicky/customer/CustomerApplicationTests.java
@@ -2,6 +2,7 @@ package com.nicky.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
 class CustomerApplicationTests {
